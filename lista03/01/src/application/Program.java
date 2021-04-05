67030b941d523dda773851382f59b7af466d80a0
Emanuel Martins <emanuelmartins284@gmail.com>
2021-04-05 16:05:16 -0300

    updt: first poo exercise

---
+++ lista03/01/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+
+import entities.Rectangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    /*
+    Fazer um programa para ler os valores da largura e altura
+    de um retângulo. Em seguida, mostrar na tela o valor de
+    sua área, perímetro e diagonal. Usar uma classe como
+    mostrado no projeto ao lado.
+     */
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rectangle rectangle = new Rectangle();
+
+        System.out.println("Enter the width and height of thee rectangle: ");
+        rectangle.width = sc.nextDouble();
+        rectangle.height = sc.nextDouble();
+
+        System.out.println(rectangle);
+
+    }
+}
